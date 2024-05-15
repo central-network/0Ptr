af6066dab39f73730fbca5d97f74a3966c179175
Özgür, Dr. <ozgur@phy-x.net>
2024-05-15 15:32:35 +0300

    new

--- 0ptr2.js
+++ 0ptr2.js
@@ -1,10 +1,70 @@
-var BPE, POINTER_BYTELENGTH, POINTER_LENGTH, PTR_BYTELENGTH, PTR_BYTEOFFSET, PTR_CLASSINDEX, Pointer, Storage, debug, define, dvw, error, getByteOffset, getClassIndex, getPtriFloat32, getPtriUint32, iLE, log, malloc, palloc, ptri, sab, scope, setByteOffset, setClassIndex, setPtriFloat32, setPtriUint32, table, u32, warn;
+//? hello world <3
+var BPE, Class, POINTER_BYTELENGTH, POINTER_LENGTH, PTR_BYTELENGTH, PTR_BYTEOFFSET, PTR_CLASSINDEX, PTR_PARENT, Property, Storage, addChildren, className, classes, d, debug, decode, define, dvw, encode, error, get, getByteLength, getByteOffset, getClassIndex, getParent, getPtriFloat32, getPtriUint32, getown, iLE, key, log, malloc, name, new_Pointer, new_Uint32Array, new_Uint8Array, p0, p1, palloc, prop, ptr_Pointer, rc, sab, set, setByteLength, setByteOffset, setClassIndex, setParent, setPtriFloat32, setPtriUint32, sliceUint8, storeForUint32, storeForUint8, subarrayUint32, subarrayUint8, table, u32, ui8, warn;
 
+export var Pointer = class Pointer extends Number {};
+
+export var Scene = class Scene extends Pointer {};
+
+export var DrawCall = class DrawCall extends Pointer {};
+
+export var Viewport = class Viewport extends Pointer {};
+
+export var ClearColor = class ClearColor extends Pointer {};
+
+export var ClearMask = class ClearMask extends Pointer {};
+
+export var Color = class Color extends Pointer {};
+
+export var Scale = class Scale extends Pointer {};
+
+export var Rotation = class Rotation extends Pointer {};
+
+export var Position = class Position extends Pointer {};
+
+export var Vertices = class Vertices extends Pointer {};
+
+export var Mesh = class Mesh extends Pointer {};
+
+export var Text = class Text extends Pointer {};
+
+export var Id = class Id extends Text {};
+
+export var VertexShader = class VertexShader extends Text {};
+
+export var ComputeShader = class ComputeShader extends Text {};
+
+export var FragmentShader = class FragmentShader extends Text {};
+
+export var Program = class Program extends Pointer {};
+
+export var EventHandler = class EventHandler extends Pointer {};
+
+export var RenderingContext = class RenderingContext extends Pointer {};
+
+export var DrawBuffer = class DrawBuffer extends Pointer {};
+
+export var VertexArray = class VertexArray extends Pointer {};
+
+export var Attribute = class Attribute extends Text {};
+
+export var Uniform = class Uniform extends Text {};
+
+export var CPU = class CPU extends Text {};
+
+export var GPU = class GPU extends Pointer {};
+
+export var AllocArray = class AllocArray extends Pointer {};
+
+export default classes = new Object({Scene, DrawCall, Viewport, ClearColor, ClearMask, Color, Scale, Rotation, Position, Vertices, Mesh, Id, VertexShader, FragmentShader, EventHandler, Program, RenderingContext, VertexArray, Attribute, Uniform, CPU, GPU, AllocArray});
+
+//* export|class|extends|Pointer|Number|Text|\s+
 ({log, warn, error, table, debug} = console);
 
 sab = new SharedArrayBuffer(1e7);
 
 dvw = new DataView(sab);
+
+ui8 = new Uint8Array(sab);
 
 u32 = new Uint32Array(sab);
 
@@ -12,24 +72,47 @@ iLE = new Uint8Array(Uint16Array.of(1).buffer)[0] === 1;
 
 BPE = 4;
 
-scope = new (Storage = class Storage extends Array {
+POINTER_LENGTH = 16;
+
+POINTER_BYTELENGTH = BPE * POINTER_LENGTH;
+
+PTR_CLASSINDEX = 0 * BPE;
+
+PTR_PARENT = 1 * BPE;
+
+PTR_BYTEOFFSET = 2 * BPE;
+
+PTR_BYTELENGTH = 3 * BPE;
+
+//* laskdşlkalsşkdşalkdşlaskdşlaskd
+palloc = Atomics.add.bind(Atomics, u32, 0, POINTER_BYTELENGTH);
+
+malloc = Atomics.add.bind(Atomics, u32, 1);
+
+palloc(malloc(POINTER_BYTELENGTH * 1e5));
+
+define = Object.defineProperties;
+
+getown = Object.getOwnPropertyDescriptor;
+
+encode = TextEncoder.prototype.encode.bind(new TextEncoder);
+
+decode = TextDecoder.prototype.decode.bind(new TextDecoder);
+
+export var PtriArray = class PtriArray extends Array {};
+
+export var storage = new (Storage = class Storage extends Array {
   constructor() {
     super(...arguments).add(null);
   }
 
-  classIndex() {
-    return this.indexOf(arguments[0].constructor);
+  findByName() {
+    return this.find((i) => {
+      return i && i.name === arguments[0];
+    });
   }
 
-  store(o) {
-    var i;
-    if (-1 === (i = this.indexOf(o))) {
-      i += this.push(o);
-    }
-    return i;
-  }
-
-  fillEmpty(o) {
+  fillFirstEmpty(o) {
     var i;
     this[i = this.findIndex(function(v) {
       return !v;
@@ -37,30 +120,22 @@ scope = new (Storage = class Storage extends Array {
     return i;
   }
 
-  add(o) {
-    return this[this.length] = o;
+  pushOrFindIndex(o) {
+    var i;
+    if (-1 === (i = this.indexOf(o))) {
+      i += this.push(o);
+    }
+    return i;
   }
 
-});
+  add(o) {
+    this[this.length] = o;
+    return this;
+  }
 
-POINTER_LENGTH = 16;
+})(0xff);
 
-POINTER_BYTELENGTH = BPE * POINTER_LENGTH;
-
-define = Object.defineProperties;
-
-palloc = Atomics.add.bind(Atomics, u32, 0, POINTER_BYTELENGTH);
-
-malloc = Atomics.add.bind(Atomics, u32, 1);
-
-palloc(malloc(POINTER_BYTELENGTH));
-
-PTR_CLASSINDEX = 0 * BPE;
-
-PTR_BYTEOFFSET = 1 * BPE;
-
-PTR_BYTELENGTH = 2 * BPE;
-
+//* lşasdklkasşdkaşsldkşasldkşalsdkasşlkdlşsakd
 getByteOffset = function(ptri) {
   return dvw.getUint32(ptri + PTR_BYTEOFFSET, iLE);
 };
@@ -70,13 +145,51 @@ setByteOffset = function(ptri, byteOffset) {
   return ptri;
 };
 
+getByteLength = function(ptri) {
+  return dvw.getUint32(ptri + PTR_BYTELENGTH, iLE);
+};
+
+setByteLength = function(ptri, byteLength) {
+  dvw.setUint32(ptri + PTR_BYTELENGTH, byteLength, iLE);
+  return ptri;
+};
+
+ptr_Pointer = function(ptri) {
+  return ptri && new storage[getClassIndex(ptri)](ptri);
+};
+
+new_Pointer = function(Class) {
+  var byteLength, ptri;
+  setClassIndex(ptri = new Class(palloc()));
+  if (!(byteLength = Class.byteLength)) {
+    return ptri;
+  }
+  setByteOffset(ptri, malloc(byteLength));
+  setByteLength(ptri, byteLength);
+  return ptri;
+};
+
 getClassIndex = function(ptri) {
-  return dvw.getUint32(ptri + PTR_CLASSINDEX, iLE);
+  return dvw.getUint32(ptri + PTR_CLASSINDEX, iLE) || storage.indexOf(ptri.constructor);
 };
 
 setClassIndex = function(ptri, clsi) {
-  dvw.setUint32(ptri + PTR_CLASSINDEX, clsi || scope.classIndex(ptri), iLE);
+  dvw.setUint32(ptri + PTR_CLASSINDEX, clsi || getClassIndex(ptri), iLE);
   return ptri;
+};
+
+addChildren = function(parent, child) {
+  dvw.setUint32(child + PTR_PARENT, parent, iLE);
+  return child;
+};
+
+setParent = function(child, parent) {
+  dvw.setUint32(child + PTR_PARENT, parent, iLE);
+  return parent;
+};
+
+getParent = function(ptri) {
+  return dvw.getUint32(ptri + PTR_PARENT, iLE);
 };
 
 getPtriUint32 = function(ptri, byteOffset) {
@@ -97,54 +210,305 @@ setPtriFloat32 = function(ptri, byteOffset, value) {
   return value;
 };
 
-setByteOffset(32, 64);
+storeForUint8 = function(any) {
+  return storage.fillFirstEmpty(any);
+};
 
-scope.store(Pointer = (function() {
-  class Pointer extends Number {
-    static from(any) {
-      var ptri;
-      return setClassIndex(ptri = new this(palloc()));
+storeForUint32 = function(any) {
+  return storage.pushOrFindIndex(any);
+};
+
+new_Uint32Array = function(ptri, byteOffset, length) {
+  length || (length = getByteLength(ptri) / 4);
+  byteOffset = getByteOffset(ptri) + byteOffset || 0;
+  return new Uint32Array(sab, byteOffset, length);
+};
+
+new_Uint8Array = function(ptri, byteOffset, length) {
+  length || (length = getByteLength(ptri));
+  byteOffset = getByteOffset(ptri) + byteOffset || 0;
+  return new Uint8Array(sab, byteOffset, length);
+};
+
+subarrayUint8 = function(ptri, begin, end) {
+  var length, offset;
+  offset = getByteOffset(ptri);
+  length = getByteLength(ptri);
+  end || (end = length + (begin || (begin = begin || 0)));
+  return ui8.subarray(begin + offset, end + offset);
+};
+
+sliceUint8 = function(ptri, begin, end) {
+  var length, offset;
+  offset = getByteOffset(ptri);
+  length = getByteLength(ptri);
+  end || (end = length + (begin || (begin = begin || 0)));
+  return ui8.slice(begin + offset, end + offset);
+};
+
+subarrayUint32 = function(ptri, begin, end) {
+  var length, offset;
+  offset = getByteOffset(ptri) / 4;
+  length = getByteLength(ptri) / 4;
+  end || (end = length + (begin || (begin = begin || 0)));
+  return u32.subarray(begin + offset, end + offset);
+};
+
+define(Pointer.prototype, {
+  ['{{Pointer}}']: {
+    get: function() {
+      return new Uint32Array(sab, this, POINTER_LENGTH);
     }
+  }
+});
 
-    store(object) {
-      var i;
-      if (!/WebGL/i.test(object.constructor.name)) {
-        return this.storage.store(object);
+define(Pointer, {
+  from: {
+    value: function() {
+      var arg0, i, j, len, prop, ptri, ref, value;
+      setClassIndex(ptri = new this(palloc()));
+      switch ((ref = (arg0 = arguments[0])) != null ? ref.constructor : void 0) {
+        case Object:
+          for (prop in arg0) {
+            value = arg0[prop];
+            addChildren(ptri, storage[prop].from(value));
+          }
+          break;
+        case Array:
+          for (j = 0, len = arg0.length; j < len; j++) {
+            i = arg0[j];
+            for (prop in i) {
+              value = i[prop];
+              addChildren(ptri, storage[prop].from(value));
+            }
+          }
+          break;
+        case String:
+          ptri.set(arg0);
+          break;
+        default:
+          error(arg0);
       }
-      if (0xff < (i = this.storage.fillEmpty(object))) {
+      return ptri;
+    }
+  }
+});
+
+define(Pointer.prototype, {
+  toString: {
+    value: function() {
+      return error("tostring", this);
+    }
+  }
+});
+
+define(Pointer.prototype, {
+  add: {
+    value: function(ptri) {
+      return setParent(ptri, this);
+    }
+  }
+});
+
+define(Pointer.prototype, {
+  append: {
+    value: function(ptri) {
+      return addChildren(this, ptri);
+    }
+  }
+});
+
+define(Pointer.prototype, {
+  children: {
+    enumerable: true,
+    get: function() {
+      var list, ptri, ptrj;
+      ptrj = dvw.getUint32(0, iLE);
+      ptri = +this;
+      list = new PtriArray;
+      while (ptrj -= POINTER_BYTELENGTH) {
+        if (ptri - getParent(ptrj)) {
+          continue;
+        }
+        list[list.length] = ptr_Pointer(ptrj);
+      }
+      return list;
+    }
+  }
+});
+
+define(Pointer.prototype, {
+  parent: {
+    enumerable: true,
+    get: function() {
+      return ptr_Pointer(getParent(this));
+    }
+  }
+});
+
+define(Pointer.prototype, {
+  isPointer: {
+    value: true
+  }
+});
+
+define(Pointer.prototype, {
+  on: {
+    value: function(event, handler) {
+      return this;
+    }
+  }
+});
+
+define(Pointer.prototype, {
+  once: {
+    value: function(event, handler) {
+      return this;
+    }
+  }
+});
+
+define(Pointer.prototype, {
+  emit: {
+    value: function(event, handler) {
+      return this;
+    }
+  }
+});
+
+define(Text.prototype, {
+  TypedArray: {
+    value: Uint8Array
+  }
+});
+
+define(Text.prototype, {
+  text: {
+    enumerable: true,
+    get: function() {
+      return decode(sliceUint8(this));
+    }
+  }
+});
+
+define(Text.prototype, {
+  set: {
+    value: function(value) {
+      var byteLength, byteOffset;
+      if (/string/.test(typeof value)) {
+        value = encode(value);
+      }
+      if (!value instanceof Uint8Array) {
         throw {
-          MAX_INDEX_EXCEED: object
+          TEXT_VALUE_ERROR: value
         };
       }
-      return i;
+      if (!(byteOffset = getByteOffset(this))) {
+        byteLength = value.byteLength;
+        byteOffset = malloc(byteLength);
+        setByteOffset(this, byteOffset);
+        setByteLength(this, byteLength);
+      }
+      ui8.set(value, byteOffset);
+      return this;
     }
+  }
+});
 
-  };
+define(Color, {
+  byteLength: {
+    value: 4 * 4
+  }
+});
 
-  define(Pointer.prototype, {
-    isPointer: {
-      value: true
+define(Color.prototype, {
+  TypedArray: {
+    value: Float32Array
+  }
+});
+
+define(RenderingContext.prototype, {
+  getViewport: {
+    value: function() {
+      return new_Pointer(Viewport);
+    }
+  }
+});
+
+//? <------->
+for (name in classes) {
+  Class = classes[name];
+  prop = name[0].toLowerCase() + name.substring(1);
+  define(storage.add(Class), {
+    [prop]: {
+      value: Class
     }
   });
-
-  define(Pointer.prototype, {
-    storage: {
-      value: new Storage(0xff)
+  for (name in Object.getOwnPropertyDescriptors(Class.prototype)) {
+    if (!/get|set/.test(key = name.substring(0, 3))) {
+      continue;
+    }
+    if (!(className = name.substring(3))) {
+      continue;
+    }
+    if (!(Property = storage.findByName(className))) {
+      continue;
+    }
+    if (false !== Object.hasOwn(Class.prototype, prop = className[0].toLowerCase() + className.substring(1))) {
+      continue;
+    }
+    if (d = getown(Class.prototype, `get${className}`)) {
+      get = d.value;
+    }
+    if (d = getown(Class.prototype, `set${className}`)) {
+      set = d.value;
+    }
+    define(Class.prototype, {
+      [prop]: {
+        get,
+        set,
+        enumerable: true
+      }
+    });
+  }
+  if (!Class.prototype.TypedArray) {
+    continue;
+  }
+  define(Class.prototype, {
+    BYTES_PER_ELEMENT: {
+      value: Class.prototype.TypedArray.BYTES_PER_ELEMENT
     }
   });
+  if (Class.byteLength) {
+    define(Class, {
+      length: {
+        value: Class.byteLength / Class.prototype.BYTES_PER_ELEMENT
+      }
+    });
+  }
+  continue;
+}
 
-  return Pointer;
+//? <------->
+warn(rc = new_Pointer(RenderingContext));
 
-}).call(this));
+warn(p0 = Program.from({
+  vertexShader: "hello world vs"
+}));
 
-warn(ptri = Pointer.from());
+warn(p1 = Program.from([
+  {
+    fragmentShader: "hello world fs"
+  },
+  {
+    vertexShader: "hello world vs"
+  }
+]));
 
-warn(ptri.store(document));
+warn("p0.add p1:", p0.add(p1));
 
-warn(ptri.store(new OffscreenCanvas(1, 1).getContext("webgl2")));
+warn("p0.append p1:", p0.append(p1));
 
-warn(ptri.store(new OffscreenCanvas(1, 1).getContext("webgl2").createProgram()));
+warn("rc.append p0:", rc.append(p0));
 
-log(setPtriUint32(4, 12, ptri));
-
-log(getPtriUint32(4, ptri));
+warn("rc:", rc);
--- coffee/0ptr2.coffee
+++ coffee/0ptr2.coffee
@@ -1,29 +1,83 @@
+#? hello world <3
+
+export class Pointer            extends Number
+export class Scene              extends Pointer
+export class DrawCall           extends Pointer
+export class Viewport           extends Pointer
+export class ClearColor         extends Pointer
+export class ClearMask          extends Pointer
+export class Color              extends Pointer
+export class Scale              extends Pointer
+export class Rotation           extends Pointer
+export class Position           extends Pointer
+export class Vertices           extends Pointer
+export class Mesh               extends Pointer
+export class Text               extends Pointer
+export class Id                 extends Text
+export class VertexShader       extends Text
+export class ComputeShader      extends Text
+export class FragmentShader     extends Text
+export class Program            extends Pointer
+export class EventHandler       extends Pointer
+export class RenderingContext   extends Pointer
+export class DrawBuffer         extends Pointer
+export class VertexArray        extends Pointer
+export class Attribute          extends Text
+export class Uniform            extends Text
+export class CPU                extends Text
+export class GPU                extends Pointer
+export class AllocArray         extends Pointer
+
+
+export default classes = new Object {
+    Scene, DrawCall, Viewport, ClearColor, ClearMask, 
+    Color, Scale, Rotation, Position, Vertices, 
+    Mesh, Id, VertexShader, FragmentShader, EventHandler,
+    Program, RenderingContext, VertexArray, Attribute, 
+    Uniform, CPU, GPU, AllocArray
+}
+
+#* export|class|extends|Pointer|Number|Text|\s+
+
 { log, warn, error, table, debug } = console
 
 sab = new SharedArrayBuffer 1e7
 dvw = new DataView sab
+ui8 = new Uint8Array sab
 u32 = new Uint32Array sab
 iLE = new Uint8Array(Uint16Array.of(1).buffer)[0] is 1
 BPE = 4
 
-scope = new class Storage extends Array
-    constructor : -> super( arguments... ).add null
-    classIndex  : -> @indexOf arguments[0].constructor
-    store       : (o) -> i += @push o if -1 is i = @indexOf o ; i
-    fillEmpty   : (o) -> @[ i = @findIndex((v) -> !v) ] = o ; i
-    add         : (o) -> @[ @length ] = o
-
 POINTER_LENGTH      = 16
 POINTER_BYTELENGTH  = BPE * POINTER_LENGTH
 
-define = Object.defineProperties
+PTR_CLASSINDEX      = 0 * BPE
+PTR_PARENT          = 1 * BPE
+PTR_BYTEOFFSET      = 2 * BPE
+PTR_BYTELENGTH      = 3 * BPE
+
+#* laskdşlkalsşkdşalkdşlaskdşlaskd
+
 palloc = Atomics.add.bind Atomics, u32, 0, POINTER_BYTELENGTH
 malloc = Atomics.add.bind Atomics, u32, 1
-palloc malloc POINTER_BYTELENGTH
 
-PTR_CLASSINDEX  = 0 * BPE
-PTR_BYTEOFFSET  = 1 * BPE
-PTR_BYTELENGTH  = 2 * BPE
+palloc malloc POINTER_BYTELENGTH * 1e5
+
+define = Object.defineProperties
+getown = Object.getOwnPropertyDescriptor
+encode = TextEncoder::encode.bind new TextEncoder
+decode = TextDecoder::decode.bind new TextDecoder
+
+export class PtriArray extends Array
+
+export storage = new (class Storage extends Array
+    constructor     : -> super( arguments... ).add null
+    findByName      : -> @find((i) => i and i.name is arguments[0])
+    fillFirstEmpty  : (o) -> @[ i = @findIndex((v) -> !v) ] = o ; i
+    pushOrFindIndex : (o) -> i += @push o if -1 is i = @indexOf o ; i
+    add             : (o) -> @[ @length ] = o ; this ) 0xff
+
+#* lşasdklkasşdkaşsldkşasldkşalsdkasşlkdlşsakd
 
 getByteOffset   = ( ptri ) ->
     dvw.getUint32 ptri + PTR_BYTEOFFSET, iLE
@@ -31,13 +85,41 @@ getByteOffset   = ( ptri ) ->
 setByteOffset   = ( ptri, byteOffset ) ->
     dvw.setUint32 ptri + PTR_BYTEOFFSET, byteOffset, iLE ; ptri
 
+getByteLength   = ( ptri ) ->
+    dvw.getUint32 ptri + PTR_BYTELENGTH, iLE
+
+setByteLength   = ( ptri, byteLength ) ->
+    dvw.setUint32 ptri + PTR_BYTELENGTH, byteLength, iLE ; ptri
+
+ptr_Pointer     = ( ptri ) ->
+    ptri and new storage[ getClassIndex ptri ] ptri
+
+new_Pointer     = ( Class ) ->
+    setClassIndex ptri = new Class palloc()
+    return ptri if !byteLength = Class.byteLength
+
+    setByteOffset ptri, malloc byteLength
+    setByteLength ptri, byteLength
+
+    return ptri
+
 getClassIndex   = ( ptri ) ->
-    dvw.getUint32 ptri + PTR_CLASSINDEX, iLE
+    dvw.getUint32( ptri + PTR_CLASSINDEX, iLE ) or
+    storage.indexOf( ptri.constructor )
 
 setClassIndex   = ( ptri, clsi ) ->
     dvw.setUint32( ptri + PTR_CLASSINDEX,
-        clsi or scope.classIndex( ptri ), iLE
+        clsi or getClassIndex( ptri ), iLE
     ) ; ptri
+
+addChildren     = ( parent, child ) ->
+    dvw.setUint32 child + PTR_PARENT, parent, iLE ; child
+
+setParent       = ( child, parent ) ->
+    dvw.setUint32 child + PTR_PARENT, parent, iLE ; parent
+
+getParent       = ( ptri ) ->
+    dvw.getUint32 ptri + PTR_PARENT, iLE
 
 getPtriUint32   = ( ptri, byteOffset ) ->
     dvw.getUint32 byteOffset + getByteOffset( ptri ), iLE
@@ -51,32 +133,166 @@ getPtriFloat32  = ( ptri, byteOffset ) ->
 setPtriFloat32  = ( ptri, byteOffset, value ) ->
     dvw.setFloat32 byteOffset + getByteOffset( ptri ), value, iLE ; value
 
-setByteOffset 32, 64
+storeForUint8   = ( any ) ->
+    storage.fillFirstEmpty any
 
-scope.store class Pointer extends Number
+storeForUint32  = ( any ) ->
+    storage.pushOrFindIndex any
 
-    define this::, isPointer : value : on
+new_Uint32Array = ( ptri, byteOffset, length ) ->
+    length ||= getByteLength( ptri ) / 4
+    byteOffset = getByteOffset( ptri ) + byteOffset || 0
 
-    define this::, storage   : value : new Storage(0xff)
-    
-    @from       : ( any ) ->
+    new Uint32Array sab, byteOffset, length
+
+new_Uint8Array  = ( ptri, byteOffset, length ) ->
+    length ||= getByteLength( ptri )
+    byteOffset = getByteOffset( ptri ) + byteOffset || 0
+
+    new Uint8Array sab, byteOffset, length
+
+subarrayUint8   = ( ptri, begin, end ) ->
+    offset = getByteOffset( ptri )
+    length = getByteLength( ptri )
+
+    end ||= length + begin ||= begin or 0
+    ui8.subarray begin + offset, end + offset 
+
+sliceUint8      = ( ptri, begin, end ) ->
+    offset = getByteOffset( ptri )
+    length = getByteLength( ptri )
+
+    end ||= length + begin ||= begin or 0
+    ui8.slice begin + offset, end + offset 
+
+subarrayUint32  = ( ptri, begin, end ) ->
+    offset = getByteOffset( ptri ) / 4
+    length = getByteLength( ptri ) / 4
+
+    end ||= length + begin ||= begin or 0
+    u32.subarray begin + offset, end + offset 
+
+
+define Pointer::            , [ '{{Pointer}}' ] :
+    get : -> new Uint32Array sab, this, POINTER_LENGTH    
+
+define Pointer              , from              :
+    value : ->
         setClassIndex ptri = new this palloc()
 
-    store       : ( object ) ->
-        if !/WebGL/i.test object.constructor.name
-            return @storage.store object
+        switch ( arg0 = arguments[0] )?.constructor
+            when Object then for prop, value of arg0
+                addChildren ptri, storage[ prop ].from value
 
-        if  0xff < i = @storage.fillEmpty object
-            throw MAX_INDEX_EXCEED : object
+            when Array then for i in arg0 then for prop, value of i
+                addChildren ptri, storage[ prop ].from value
 
-        ;i
+            when String then ptri.set arg0
+
+            else error arg0
+
+        return ptri
+define Pointer::            , toString          :
+    value : -> error "tostring", this
+define Pointer::            , add               :
+    value : ( ptri ) -> setParent ptri, this
+define Pointer::            , append            :
+    value : ( ptri ) -> addChildren this, ptri
+define Pointer::            , children          :
+    enumerable: on,
+    get   : ->
+        ptrj = dvw.getUint32 0, iLE
+        ptri = +this
+        list = new PtriArray
+
+        while ptrj -= POINTER_BYTELENGTH
+            continue if ptri - getParent ptrj
+            list[ list.length ] = ptr_Pointer ptrj
+
+        list
+define Pointer::            , parent            :
+    enumerable: on,
+    get   : -> ptr_Pointer getParent this
+define Pointer::            , isPointer         :
+    value : true
+define Pointer::            , on                :
+    value : ( event, handler ) -> this
+define Pointer::            , once              :
+    value : ( event, handler ) -> this
+define Pointer::            , emit              :
+    value : ( event, handler ) -> this
+define Text::               , TypedArray        :
+    value : Uint8Array
+define Text::               , text              :
+    enumerable : on
+    get   : -> decode sliceUint8 this 
+define Text::               , set               :
+    value : ( value ) ->  
+
+        if  /string/.test typeof value
+            value = encode value
+
+        if !value instanceof Uint8Array
+            throw TEXT_VALUE_ERROR : value
+
+        if !byteOffset = getByteOffset this
+            byteLength = value .byteLength
+            byteOffset = malloc byteLength
+
+            setByteOffset this, byteOffset
+            setByteLength this, byteLength
+
+
+        ui8.set( value, byteOffset )
+        
+        ;@
+define Color                , byteLength        :
+    value : 4 * 4
+define Color::              , TypedArray        :
+    value : Float32Array
+define RenderingContext::   , getViewport       :
+    value : -> new_Pointer Viewport
+
+#? <------->
+
+
+for name , Class of classes
+
+    prop = name[0].toLowerCase() + name.substring 1
+    define storage.add( Class ), [ prop ] : { value : Class }
     
+    for name of Object.getOwnPropertyDescriptors Class::
+
+        continue unless /get|set/.test key = name.substring 0, 3
+        continue unless className = name.substring 3
+        continue unless Property = storage.findByName className
+        continue unless no is Object.hasOwn( Class::, prop =
+            className[0].toLowerCase() + className.substring 1 )
+
+        get = d.value if d = getown Class::, "get#{className}"
+        set = d.value if d = getown Class::, "set#{className}"
+
+        define Class::, [ prop ] : { get, set, enumerable : on }
+    
+    continue unless Class::TypedArray
+
+    define( Class::, BYTES_PER_ELEMENT : {
+        value : Class::TypedArray.BYTES_PER_ELEMENT
+    } )
+    
+    define( Class, length : value : (
+        Class.byteLength / Class::BYTES_PER_ELEMENT
+    ) ) if Class.byteLength
+
+    continue
+
+#? <------->
 
 
-warn ptri = Pointer.from()
-warn ptri.store( document )
-warn ptri.store( new OffscreenCanvas(1,1).getContext("webgl2") )
-warn ptri.store( new OffscreenCanvas(1,1).getContext("webgl2").createProgram() )
-
-log setPtriUint32 4, 12, ptri
-log getPtriUint32 4, ptri
+warn rc = new_Pointer( RenderingContext )
+warn p0 = Program.from({ vertexShader : "hello world vs" })
+warn p1 = Program.from([ { fragmentShader : "hello world fs" }, { vertexShader : "hello world vs" } ])
+warn "p0.add p1:", p0.add p1
+warn "p0.append p1:", p0.append p1
+warn "rc.append p0:", rc.append p0
+warn "rc:", rc
