55226c172b8e30c572eeab06ae2b541c2563888f
Özgür, Dr. <ozgur@phy-x.net>
2024-05-25 22:02:44 +0300

    asdsd

--- 0ptr4.coffee
+++ 0ptr4.coffee
@@ -8,302 +8,102 @@ GL2NUM = new Object
 
 { log, warn, error, table, debug, info } = console
 
-sab = new SharedArrayBuffer 1e7 * 8
+sab = new SharedArrayBuffer 256
 dvw = new DataView sab
 ui8 = new Uint8Array sab
 u32 = new Uint32Array sab
 f32 = new Float32Array sab
 iLE = new Uint8Array( Uint16Array.of(1).buffer )[0] is 1
-BPE = 4
-PTR = 16 * BPE
+ref = new Array null
 
-iSTATE = 0
-iTYPE = 1
-iOFFSET = 2
-iRESv = 3
+encodeText  = TextEncoder::encode.bind new TextEncoder
+decodeText  = TextDecoder::decode.bind new TextDecoder
 
-getState  = Atomics.load.bind Atomics, ui8
-setState  = Atomics.store.bind Atomics, ui8
-getType   = ( ptri ) -> dvw.getUint8 ptri + iTYPE
-setType   = ( ptri , value ) -> dvw.setUint8 ptri + iTYPE, value
-getOffset = ( ptri ) -> dvw.getUint8 ptri + iOFFSET
-setOffset = ( ptri , value ) -> dvw.setUint8 ptri + iOFFSET, value
-getResvi8 = ( ptri ) -> dvw.getInt8 ptri + iRESv
-setResvi8 = ( ptri , value ) -> dvw.setInt8 ptri + iRESv, value
+encodeJSON  = -> encodeText JSON.stringify arguments...
+decodeJSON  = -> JSON.parse decodeText arguments...
 
-PTRTYPE =
-    PTRTYPE_CLASS : PTRTYPE_CLASS = 1
-    PTRTYPE_OBJECT : PTRTYPE_OBJECT = 2
+iterate     = ( begin = 0, count = 0 ) ->
 
-PTRSTAT =
-    PTRSTAT_DELETE : PTRSTAT_DELETE = 0
-    PTRSTAT_IGNORE : PTRSTAT_IGNORE = 2
-    PTRSTAT_ACTIVE : PTRSTAT_ACTIVE = 10
-    PTRSTAT_PALLOC : PTRSTAT_PALLOC = 11
+    stored = []
+    byteOffset = begin
 
-PALLOC_TYPE =    
-    PALLOC_UINT8 : PALLOC_UINT8 = 1
-    PALLOC_UINT32 : PALLOC_UINT32 = 4
+    while byteLength = dvw.getUint32 byteOffset, iLE
 
-for c in [ PTRTYPE, PALLOC_TYPE ]
-    for key, val of c 
-        c[ val ] = val
+        object = restore byteOffset
+        encoding = dvw.getUint32 byteOffset + 4, iLE
 
-class Collection    extends Array
-
-class Storage       extends Array
-        constructor         : -> super( arguments... )[0] ?= null
-        store               : ( any, bytes = 2 ) ->
-            if  -1 is i = @indexOf any
-                
-                i = Math.pow 0xff, bytes-1
-                i = 1 + i while this[ i ]
-
-                if  i > Math.pow( 0xff, bytes )
-                    throw /EXCEED_STORAGE/
-
-                else this[ i ] = any
-
-            if  Math.pow( 0xff, bytes ) <= i
-                throw /EXCEED_STORAGE/
-
-            return i
-        storeForUint8       : ( any ) -> @store any, 1
-        storeForUint16      : ( any ) -> @store any, 2
-        storeForUint32      : ( any ) -> @store any, 4
-        storeForUint64      : ( any ) -> @store any, 8
-
-assign = Object.assign
-hasOwn = Object.hasOwn
-getOwn = Object.getOwnPropertyDescriptors
-protof = Object.getPrototypeOf
-define = ->
-    [ object, prop, desc ] = arguments
-
-    unless arguments[2]?
-        for prop, desc of { ...prop }
-            define object, prop, desc
-        return object
-
-    if !desc.get and !desc.set and !desc.value
-        desc = value : desc
-
-    Object.defineProperty object, prop, desc
-    
-encode = TextEncoder::encode.bind new TextEncoder
-decode = TextDecoder::decode.bind new TextDecoder
-palloc = ->
-    o = Atomics.add u32, 0, PTR
-    unless o then throw [ /PALLOC/, u32.slice(0,2) ]
-    o
-#Atomics.or u32, 0, PTR
-
-
-do clearself = ->
-    for p in "
-    isFinite isInteger isNaN isSafeInteger parseFloat parseInt 
-    ".split(/\n|\s+/g) then Reflect.deleteProperty( Number, p )
-
-    for p in "
-        toExponential toLocaleString toPrecision toFixed
-    ".split(/\n|\s+/g) then Reflect.deleteProperty( Number::, p )
-
-    for p in "
-        assign create entries freeze fromEntries getOwnPropertyDescriptor
-        getOwnPropertyNames getOwnPropertySymbols getPrototypeOf
-        groupBy hasOwn is isExtensible isFrozen isSealed keys
-        preventExtensions seal setPrototypeOf values
-    ".split(/\n|\s+/g) then Reflect.deleteProperty( Object, p )
-
-    for p in "
-        __defineGetter__ __defineSetter__ __lookupGetter__ __lookupSetter__
-        propertyIsEnumerable toLocaleString hasOwnProperty
-    ".split(/\n|\s+/g) then Reflect.deleteProperty( Object::, p )    
-
-    0
-
-
-define window   , storage   : new Storage Number
-define Number:: , toString  : -> throw /toString/
-
-register = ->
-    for Alias, Parent of arguments[0]
-
-        if  -1 is i = storage.indexOf Parent
-            throw /PARENT_NOT_REGISTERED/
-
-        if  storage.find (c) -> c?.name is Alias
-            throw /ALREADY_REGISTERED/
-
-        storagei = storage.storeForUint8( Alias )
-
-        document.head.appendChild(
-            assign(document.createElement("script"), { text :
-                "#{Alias} = class extends storage[#{i}] {};
-                 storage[ #{storagei} ] = #{Alias};"
-            })
-        )#.remove()
-
-    0
-
-register Pointer    : Number
-
-###        
-
-register Class      : Pointer
-register Allocation : Pointer
-register Vector3    : Pointer
-
-
-
-
-define Class::      ,
-
-    prototype       :
-        enumerable  : on
-        get : -> storage.find ({ clsptri }) =>
-            ( clsptri - this ) is 0
-
-    name            :
-        enumerable  : on
-        get : -> @prototype.name
-
-    parent          :
-        enumerable  : on
-        get : ->
-            protof( @prototype )["{{Class}}"] or new Number 0           
-
-    keyName         : get : ->
-        name = @name
-        if  name.split("").some (c) -> c is c.toUpperCase()
-            return name.toLocaleLowerCase()
-        name[0].toLowerCase() + name.substring 1
-
-define Pointer , alloc : get : ->
-    ptri = new this palloc()
-    dvw.setUint8 ptri + 0, 1 #status
-    dvw.setUint8 ptri + 1, 4 #inner alloc offset
-
-    ->
-        ptri
-###
-
-parse = {}
-
-# type 2 is a property definition
-parse[ TYPEOF_PROPERTY_DEFINITION = 2 ] = ( offset ) ->
-
-    byteOffset  = offset * 4
-    byteLength  = u32[ offset++ ] * 4
-    type        = u32[ offset++ ]
-
-    #? read propname bytelength
-    propByteLength      = u32[ offset++ ] # next of type 
-
-    #? read options bytelength
-    optionsByteLength   = u32[ offset++ ] # next of namesize
-
-    propBegin           = offset * 4
-    propEnd             = propBegin + propByteLength
-
-    optionsBegin        = propEnd
-    optionsEnd          = optionsBegin + optionsByteLength
-
-    #? decode name
-    prop                = decode ui8.slice propBegin, propEnd
-    options             = decode ui8.slice optionsBegin, optionsEnd
-    
-    return {
-        byteOffset , byteLength ,
-        type , name : prop,
-        data : JSON.parse options
-    }
-
-findProperty = ( prop ) ->
-
-    offset = 0
-    while length = u32[ offset ]
-        #* readed bytelength belongs to all packet
-
-        if  TYPEOF_PROPERTY_DEFINITION is u32[ offset + 1 ]
-            property = parse[ TYPEOF_PROPERTY_DEFINITION ]( offset )
-            if  property.name is prop
-                return property
-                    
-        offset += length
-
-    null
-
-allocate = ( byteLength ) ->
-    offset = 0
-
-    while length = u32[ offset ]
-        warn { offset, length }
-        offset += length
-
-    byteLength = Math.max byteLength, 1
-
-    if  mod = byteLength % 8
-        byteLength += 8 - mod
-    
-    u32[ offset ] = byteLength / 8
-
-    offset
-
-define Pointer , defineProperty : ( prop, options ) ->
-
-        if  findProperty prop
-            throw "property is already registered: " + prop
-
-
-        nameArray = encode prop
-        optsArray = encode JSON.stringify options
-
-        type = 1
-        byteLength = 3 +
-            nameArray.byteLength + 
-            optsArray.byteLength
-
-        offset = allocate byteLength
-
-        log offset, nameArray.byteLength 
-
-        u32[ ++offset ] = type 
-        u32[ ++offset ] = nameArray.byteLength 
-        u32[ ++offset ] = optsArray.byteLength 
-
-        log { offset }, u32.slice( offset - 3, offset + 2 ) 
-
-
-        nameArrayBegin = offset * 4
-        optsArrayBegin = nameArrayBegin + nameArray.byteLength
-
-        ui8.set nameArray, nameArrayBegin
-        ui8.set optsArray, optsArrayBegin
-
-        Object.defineProperty @prototype, prop, {
-            get : ->
+        stored.push {
+            object, encoding
+            byteOffset, byteLength,
         }
 
-        log u32
-            
-        return 
+        unless --count then break
+        else byteOffset += byteLength + 8
+
+    stored
+
+bufferize   = ( object, encoding = 0 ) ->
+    ref[ encoding ].encode object
+
+objectify   = ( buffer, encoding = 0 ) ->
+    ref[ encoding ].decode buffer
+
+externref   = ( coders = { encode, decode } ) ->
+    if -1 is i = ref.indexOf coders
+        i += ref.push coders
+    i
+
+
+offset      = ->
+    byteOffset = 0
+    while len = dvw.getUint32 byteOffset, iLE
+        byteOffset += len + 8
+    byteOffset
+
+store       = ( object, encoding ) ->
+    buffer = bufferize object, encoding
+
+    byteLength = buffer.byteLength
+    byteOffset = offset()
+
+    dvw.setUint32 byteOffset, byteLength, iLE
+    dvw.setUint32 byteOffset+4, encoding, iLE
+    
+    ui8.set buffer, byteOffset+8 ; byteOffset
+
+restore     = ( byteOffset ) ->
+    
+    byteLength = dvw.getUint32 byteOffset, iLE
+    encoding = dvw.getUint32 byteOffset+4, iLE
+    buffer = new Uint8Array sab, byteOffset+8, byteLength
+
+    objectify buffer, encoding
 
 
 
+self.dump   = -> console.table iterate()
 
 
-Pointer.defineProperty "parent", {
-    size : 4, type : 2
+TYPE_TEXT   = externref {
+    encode : ( object ) -> encodeText object
+    decode : ( buffer ) -> decodeText buffer.slice()
 }
 
-warn "find:", findProperty "parent"
-
-Pointer.defineProperty "linked", {
-    size : 4, type : 3
+TYPE_JSON   = externref {
+    encode : ( object ) -> encodeJSON object
+    decode : ( buffer ) -> decodeJSON buffer.slice()
 }
 
-log new Pointer 2
-#Vector3.palloc x : PALLOC_UINT8
+warn { TYPE_TEXT, TYPE_JSON }, ref
 
-#log new Vector3 palloc()
+
+store {
+    type : 0,
+    name : "some",
+}, TYPE_JSON
+
+store "kamon", TYPE_TEXT
+
+
+log dump()
+log ui8
--- 0ptr4.js
+++ 0ptr4.js
@@ -1,41 +1,4 @@
-
-/*        
-
-register Class      : Pointer
-register Allocation : Pointer
-register Vector3    : Pointer
-
-define Class::      ,
-
-    prototype       :
-        enumerable  : on
-        get : -> storage.find ({ clsptri }) =>
-            ( clsptri - this ) is 0
-
-    name            :
-        enumerable  : on
-        get : -> @prototype.name
-
-    parent          :
-        enumerable  : on
-        get : ->
-            protof( @prototype )["{{Class}}"] or new Number 0           
-
-    keyName         : get : ->
-        name = @name
-        if  name.split("").some (c) -> c is c.toUpperCase()
-            return name.toLocaleLowerCase()
-        name[0].toLowerCase() + name.substring 1
-
-define Pointer , alloc : get : ->
-    ptri = new this palloc()
-    dvw.setUint8 ptri + 0, 1 #status
-    dvw.setUint8 ptri + 1, 4 #inner alloc offset
-
-    ->
-        ptri
-*/
-var BPE, Collection, DEBUG, GL2KEY, GL2NUM, GL2VAL, PALLOC_TYPE, PALLOC_UINT32, PALLOC_UINT8, PTR, PTRSTAT, PTRSTAT_ACTIVE, PTRSTAT_DELETE, PTRSTAT_IGNORE, PTRSTAT_PALLOC, PTRTYPE, PTRTYPE_CLASS, PTRTYPE_OBJECT, Storage, TYPEOF_PROPERTY_DEFINITION, allocate, assign, c, clearself, debug, decode, define, dvw, encode, error, f32, findProperty, getOffset, getOwn, getResvi8, getState, getType, hasOwn, iLE, iOFFSET, iRESv, iSTATE, iTYPE, info, j, key, len, log, palloc, parse, protof, ref, register, sab, setOffset, setResvi8, setState, setType, table, u32, ui8, val, warn;
+var DEBUG, GL2KEY, GL2NUM, GL2VAL, TYPE_JSON, TYPE_TEXT, bufferize, debug, decodeJSON, decodeText, dvw, encodeJSON, encodeText, error, externref, f32, iLE, info, iterate, log, objectify, offset, ref, restore, sab, store, table, u32, ui8, warn;
 
 DEBUG = 0;
 
@@ -48,7 +11,7 @@ GL2NUM = new Object;
 
 ({log, warn, error, table, debug, info} = console);
 
-sab = new SharedArrayBuffer(1e7 * 8);
+sab = new SharedArrayBuffer(256);
 
 dvw = new DataView(sab);
 
@@ -60,323 +23,112 @@ f32 = new Float32Array(sab);
 
 iLE = new Uint8Array(Uint16Array.of(1).buffer)[0] === 1;
 
-BPE = 4;
+ref = new Array(null);
 
-PTR = 16 * BPE;
+encodeText = TextEncoder.prototype.encode.bind(new TextEncoder);
 
-iSTATE = 0;
+decodeText = TextDecoder.prototype.decode.bind(new TextDecoder);
 
-iTYPE = 1;
-
-iOFFSET = 2;
-
-iRESv = 3;
-
-getState = Atomics.load.bind(Atomics, ui8);
-
-setState = Atomics.store.bind(Atomics, ui8);
-
-getType = function(ptri) {
-  return dvw.getUint8(ptri + iTYPE);
+encodeJSON = function() {
+  return encodeText(JSON.stringify(...arguments));
 };
 
-setType = function(ptri, value) {
-  return dvw.setUint8(ptri + iTYPE, value);
+decodeJSON = function() {
+  return JSON.parse(decodeText(...arguments));
 };
 
-getOffset = function(ptri) {
-  return dvw.getUint8(ptri + iOFFSET);
-};
-
-setOffset = function(ptri, value) {
-  return dvw.setUint8(ptri + iOFFSET, value);
-};
-
-getResvi8 = function(ptri) {
-  return dvw.getInt8(ptri + iRESv);
-};
-
-setResvi8 = function(ptri, value) {
-  return dvw.setInt8(ptri + iRESv, value);
-};
-
-PTRTYPE = {
-  PTRTYPE_CLASS: PTRTYPE_CLASS = 1,
-  PTRTYPE_OBJECT: PTRTYPE_OBJECT = 2
-};
-
-PTRSTAT = {
-  PTRSTAT_DELETE: PTRSTAT_DELETE = 0,
-  PTRSTAT_IGNORE: PTRSTAT_IGNORE = 2,
-  PTRSTAT_ACTIVE: PTRSTAT_ACTIVE = 10,
-  PTRSTAT_PALLOC: PTRSTAT_PALLOC = 11
-};
-
-PALLOC_TYPE = {
-  PALLOC_UINT8: PALLOC_UINT8 = 1,
-  PALLOC_UINT32: PALLOC_UINT32 = 4
-};
-
-ref = [PTRTYPE, PALLOC_TYPE];
-for (j = 0, len = ref.length; j < len; j++) {
-  c = ref[j];
-  for (key in c) {
-    val = c[key];
-    c[val] = val;
-  }
-}
-
-Collection = class Collection extends Array {};
-
-Storage = class Storage extends Array {
-  constructor() {
-    var base;
-    if ((base = super(...arguments))[0] == null) {
-      base[0] = null;
+iterate = function(begin = 0, count = 0) {
+  var byteLength, byteOffset, encoding, object, stored;
+  stored = [];
+  byteOffset = begin;
+  while (byteLength = dvw.getUint32(byteOffset, iLE)) {
+    object = restore(byteOffset);
+    encoding = dvw.getUint32(byteOffset + 4, iLE);
+    stored.push({object, encoding, byteOffset, byteLength});
+    if (!--count) {
+      break;
+    } else {
+      byteOffset += byteLength + 8;
     }
   }
-
-  store(any, bytes = 2) {
-    var i;
-    if (-1 === (i = this.indexOf(any))) {
-      i = Math.pow(0xff, bytes - 1);
-      while (this[i]) {
-        i = 1 + i;
-      }
-      if (i > Math.pow(0xff, bytes)) {
-        throw /EXCEED_STORAGE/;
-      } else {
-        this[i] = any;
-      }
-    }
-    if (Math.pow(0xff, bytes) <= i) {
-      throw /EXCEED_STORAGE/;
-    }
-    return i;
-  }
-
-  storeForUint8(any) {
-    return this.store(any, 1);
-  }
-
-  storeForUint16(any) {
-    return this.store(any, 2);
-  }
-
-  storeForUint32(any) {
-    return this.store(any, 4);
-  }
-
-  storeForUint64(any) {
-    return this.store(any, 8);
-  }
-
+  return stored;
 };
 
-assign = Object.assign;
-
-hasOwn = Object.hasOwn;
-
-getOwn = Object.getOwnPropertyDescriptors;
-
-protof = Object.getPrototypeOf;
-
-define = function() {
-  var desc, object, prop, ref1;
-  [object, prop, desc] = arguments;
-  if (arguments[2] == null) {
-    ref1 = {...prop};
-    for (prop in ref1) {
-      desc = ref1[prop];
-      define(object, prop, desc);
-    }
-    return object;
-  }
-  if (!desc.get && !desc.set && !desc.value) {
-    desc = {
-      value: desc
-    };
-  }
-  return Object.defineProperty(object, prop, desc);
+bufferize = function(object, encoding = 0) {
+  return ref[encoding].encode(object);
 };
 
-encode = TextEncoder.prototype.encode.bind(new TextEncoder);
-
-decode = TextDecoder.prototype.decode.bind(new TextDecoder);
-
-palloc = function() {
-  var o;
-  o = Atomics.add(u32, 0, PTR);
-  if (!o) {
-    throw [/PALLOC/, u32.slice(0, 2)];
-  }
-  return o;
+objectify = function(buffer, encoding = 0) {
+  return ref[encoding].decode(buffer);
 };
 
-//Atomics.or u32, 0, PTR
-(clearself = function() {
-  var k, l, len1, len2, len3, len4, m, n, p, ref1, ref2, ref3, ref4;
-  ref1 = "isFinite isInteger isNaN isSafeInteger parseFloat parseInt".split(/\n|\s+/g);
-  for (k = 0, len1 = ref1.length; k < len1; k++) {
-    p = ref1[k];
-    Reflect.deleteProperty(Number, p);
+externref = function(coders = {encode, decode}) {
+  var i;
+  if (-1 === (i = ref.indexOf(coders))) {
+    i += ref.push(coders);
   }
-  ref2 = "toExponential toLocaleString toPrecision toFixed".split(/\n|\s+/g);
-  for (l = 0, len2 = ref2.length; l < len2; l++) {
-    p = ref2[l];
-    Reflect.deleteProperty(Number.prototype, p);
-  }
-  ref3 = "assign create entries freeze fromEntries getOwnPropertyDescriptor getOwnPropertyNames getOwnPropertySymbols getPrototypeOf groupBy hasOwn is isExtensible isFrozen isSealed keys preventExtensions seal setPrototypeOf values".split(/\n|\s+/g);
-  for (m = 0, len3 = ref3.length; m < len3; m++) {
-    p = ref3[m];
-    Reflect.deleteProperty(Object, p);
-  }
-  ref4 = "__defineGetter__ __defineSetter__ __lookupGetter__ __lookupSetter__ propertyIsEnumerable toLocaleString hasOwnProperty".split(/\n|\s+/g);
-  for (n = 0, len4 = ref4.length; n < len4; n++) {
-    p = ref4[n];
-    Reflect.deleteProperty(Object.prototype, p);
-  }
-  return 0;
-})();
+  return i;
+};
 
-define(window, {
-  storage: new Storage(Number)
-});
+offset = function() {
+  var byteOffset, len;
+  byteOffset = 0;
+  while (len = dvw.getUint32(byteOffset, iLE)) {
+    byteOffset += len + 8;
+  }
+  return byteOffset;
+};
 
-define(Number.prototype, {
-  toString: function() {
-    throw /toString/;
+store = function(object, encoding) {
+  var buffer, byteLength, byteOffset;
+  buffer = bufferize(object, encoding);
+  byteLength = buffer.byteLength;
+  byteOffset = offset();
+  dvw.setUint32(byteOffset, byteLength, iLE);
+  dvw.setUint32(byteOffset + 4, encoding, iLE);
+  ui8.set(buffer, byteOffset + 8);
+  return byteOffset;
+};
+
+restore = function(byteOffset) {
+  var buffer, byteLength, encoding;
+  byteLength = dvw.getUint32(byteOffset, iLE);
+  encoding = dvw.getUint32(byteOffset + 4, iLE);
+  buffer = new Uint8Array(sab, byteOffset + 8, byteLength);
+  return objectify(buffer, encoding);
+};
+
+self.dump = function() {
+  return console.table(iterate());
+};
+
+TYPE_TEXT = externref({
+  encode: function(object) {
+    return encodeText(object);
+  },
+  decode: function(buffer) {
+    return decodeText(buffer.slice());
   }
 });
 
-register = function() {
-  var Alias, Parent, i, ref1, storagei;
-  ref1 = arguments[0];
-  for (Alias in ref1) {
-    Parent = ref1[Alias];
-    if (-1 === (i = storage.indexOf(Parent))) {
-      throw /PARENT_NOT_REGISTERED/;
-    }
-    if (storage.find(function(c) {
-      return (c != null ? c.name : void 0) === Alias;
-    })) {
-      throw /ALREADY_REGISTERED/;
-    }
-    storagei = storage.storeForUint8(Alias);
-    document.head.appendChild(assign(document.createElement("script"), {
-      text: `${Alias} = class extends storage[${i}] {}; storage[ ${storagei} ] = ${Alias
-//.remove()
-};`
-    }));
-  }
-  return 0;
-};
-
-register({
-  Pointer: Number
-});
-
-parse = {};
-
-// type 2 is a property definition
-parse[TYPEOF_PROPERTY_DEFINITION = 2] = function(offset) {
-  var byteLength, byteOffset, options, optionsBegin, optionsByteLength, optionsEnd, prop, propBegin, propByteLength, propEnd, type;
-  byteOffset = offset * 4;
-  byteLength = u32[offset++] * 4;
-  type = u32[offset++];
-  //? read propname bytelength
-  propByteLength = u32[offset++];
-  
-  //? read options bytelength
-  optionsByteLength = u32[offset++];
-  propBegin = offset * 4;
-  propEnd = propBegin + propByteLength;
-  optionsBegin = propEnd;
-  optionsEnd = optionsBegin + optionsByteLength;
-  //? decode name
-  prop = decode(ui8.slice(propBegin, propEnd));
-  options = decode(ui8.slice(optionsBegin, optionsEnd));
-  return {
-    byteOffset,
-    byteLength,
-    type,
-    name: prop,
-    data: JSON.parse(options)
-  };
-};
-
-findProperty = function(prop) {
-  var length, offset, property;
-  offset = 0;
-  while (length = u32[offset]) {
-    //* readed bytelength belongs to all packet
-    if (TYPEOF_PROPERTY_DEFINITION === u32[offset + 1]) {
-      property = parse[TYPEOF_PROPERTY_DEFINITION](offset);
-      if (property.name === prop) {
-        return property;
-      }
-    }
-    offset += length;
-  }
-  return null;
-};
-
-allocate = function(byteLength) {
-  var length, mod, offset;
-  offset = 0;
-  while (length = u32[offset]) {
-    warn({offset, length});
-    offset += length;
-  }
-  byteLength = Math.max(byteLength, 1);
-  if (mod = byteLength % 8) {
-    byteLength += 8 - mod;
-  }
-  u32[offset] = byteLength / 8;
-  return offset;
-};
-
-define(Pointer, {
-  defineProperty: function(prop, options) {
-    var byteLength, nameArray, nameArrayBegin, offset, optsArray, optsArrayBegin, type;
-    if (findProperty(prop)) {
-      throw "property is already registered: " + prop;
-    }
-    nameArray = encode(prop);
-    optsArray = encode(JSON.stringify(options));
-    type = 1;
-    byteLength = 3 + nameArray.byteLength + optsArray.byteLength;
-    offset = allocate(byteLength);
-    log(offset, nameArray.byteLength);
-    u32[++offset] = type;
-    u32[++offset] = nameArray.byteLength;
-    u32[++offset] = optsArray.byteLength;
-    log({offset}, u32.slice(offset - 3, offset + 2));
-    nameArrayBegin = offset * 4;
-    optsArrayBegin = nameArrayBegin + nameArray.byteLength;
-    ui8.set(nameArray, nameArrayBegin);
-    ui8.set(optsArray, optsArrayBegin);
-    Object.defineProperty(this.prototype, prop, {
-      get: function() {}
-    });
-    log(u32);
+TYPE_JSON = externref({
+  encode: function(object) {
+    return encodeJSON(object);
+  },
+  decode: function(buffer) {
+    return decodeJSON(buffer.slice());
   }
 });
 
-Pointer.defineProperty("parent", {
-  size: 4,
-  type: 2
-});
+warn({TYPE_TEXT, TYPE_JSON}, ref);
 
-warn("find:", findProperty("parent"));
+store({
+  type: 0,
+  name: "some"
+}, TYPE_JSON);
 
-Pointer.defineProperty("linked", {
-  size: 4,
-  type: 3
-});
+store("kamon", TYPE_TEXT);
 
-log(new Pointer(2));
+log(dump());
 
-//Vector3.palloc x : PALLOC_UINT8
-
-//log new Vector3 palloc()
+log(ui8);
