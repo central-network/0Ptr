1acc38767ba09586d5a0ee4749d11286d1152c05
Özgür, Dr. <ozgur@phy-x.net>
2024-05-31 17:27:59 +0300

    term

--- coffee/mac.coffee
+++ coffee/mac.coffee
@@ -1,6 +1,6 @@
 {log,warn,error,table,debug,info,delay} = console
 
-do  onpointerevents     = ->
+do  mouse = ->
     device      = new ArrayBuffer 64
     counters    = new Int32Array device, 0, 10
     positions   = new Float32Array device, 40, 6
@@ -44,7 +44,7 @@ do  onpointerevents     = ->
     clientX = dataView.getFloat32.bind dataView, offsets += 4, lendian
     clientY = dataView.getFloat32.bind dataView, offsets += 4, lendian
 
-do  onkeyboardevents    = ->
+do  keyboard = ->
 
     device      = new ArrayBuffer 144
     counters    = new Int32Array device, 0, 3
@@ -129,7 +129,7 @@ do  onkeyboardevents    = ->
     eventType = -> [ "keydown", "keyup" ][ lastEvent() ]
     activeKey = -> keys[ keyArray.findIndex (v) -> v ] or 0
 
-do  onbatteryevents     = ->
+do  battery = ->
 
     device    = new ArrayBuffer 24
     counters  = new Uint16Array device, 0, 6
@@ -171,14 +171,235 @@ do  onbatteryevents     = ->
     dischargingTime = dataView.getInt16.bind dataView, offsetDTime, lendian
     charging        = dataView.getUint8.bind dataView, offsetState
 
-do  onnetworkevents = ->
+do  netlink = ->
     
-    self.onclick = ->
-        #await navigator.bluetooth.requestDevice({acceptAllDevices: 1})
-        log 1, dev = await navigator.bluetooth.requestDevice({filters: [
-            name : "Data-9 iMac"
-        ]})
+    device      = new ArrayBuffer 16
+    counters    = new Int16Array device, 0, 4
+    dataView    = new DataView device
+    lendian     = new Uint8Array( Uint16Array.of(1).buffer )[0] is 1
 
-        try log await dev.gatt.getPrimaryService()
-        try log await dev.gatt.getPrimaryServices()
-        try log await dev.gatt.connect()
+    ionchange = 0 #triggers on three events all
+    ioffline  = 1
+    ionline   = 2
+    itype     = 3
+
+    navigator.connection.onchange = (e) ->
+        ++counters[ counters[itype] = ionchange ]
+
+        dataView.setUint8   14, navigator.onLine
+        dataView.setUint8   13, parseInt @effectiveType
+        dataView.setUint8   12, Number @saveData
+        dataView.setUint16  10, @rtt, lendian
+        dataView.setFloat32  8, @downlink, lendian
+
+        e.preventDefault()
+
+    window.addEventListener "offline", -> setTimeout ->
+        ++counters[ counters[ itype ] = ioffline ]
+
+    window.addEventListener "online", -> setTimeout ->
+        ++counters[ counters[ itype ] =  ionline ]
+
+    changeType = dataView.getUint16.bind dataView, 6, lendian
+    downlink = dataView.getFloat32.bind dataView, 8, lendian
+    rtt = dataView.getUint16.bind dataView, 10, lendian
+    saveData = dataView.getUint8.bind dataView, 12
+    effectiveType = dataView.getUint8.bind dataView, 13
+    currentState = dataView.getUint8.bind dataView, 14
+    changeTypes = [ "linkspeed", "linkdown", "linkup" ]
+
+do  fs = ->
+
+    device = new ArrayBuffer 4096 * 256 * 128 #128mb önbellek
+
+    counters    = new Int32Array device, 0, 4
+    info        = new DataView device, counters.byteOffset, 4
+    
+    dataArray   = new Uint8Array device, info.byteOffset
+    dataView    = new DataView device 
+    lendian     = new Uint8Array( Uint16Array.of(1).buffer )[0] is 1
+    handles     = [,]
+    root        = null
+    currentDir  = null
+
+    quota       = dataView.getInt32.bind dataView, 12, lendian
+    usage       = dataView.getInt32.bind dataView, 16, lendian
+    write       = dataView.getInt32.bind dataView, 20, lendian
+    read        = dataView.getInt32.bind dataView, 24, lendian
+    create      = dataView.getInt32.bind dataView, 28, lendian
+
+    state       = dataView.getUint8.bind dataView, 32
+    status      = dataView.getUint8.bind dataView, 34
+
+    STATE_INIT               = 0
+    STATE_ROOT_HANDLE        = 1
+    STATE_UNPERSISTED_HANDLE = 2
+    STATE_PERSISTED_HANDLE   = 3
+
+    STATUS_IDLE     = 0
+    STATUS_READING  = 1
+    STATUS_WRITING  = 2
+
+    events      = '
+    onstorageroothandle
+    onstoragepersisted
+    onstoragecreatedirectory
+    storagecreatefile
+    '.split( /\s+|\n/g )
+
+    emit = ( type, detail = {} ) ->
+        window.dispatchEvent new CustomEvent type, { detail }
+
+    window.addEventListener "storagecreatedirectory", ({ detail }) ->
+        dataView.setInt32 28, create()+1, lendian
+        log "onstoragecreatedirectory:", detail
+
+    window.addEventListener "storagecreatefile", ({ detail }) ->
+        dataView.setInt32 28, create()+1, lendian
+        log "onstoragecreatefile:", detail
+
+    window.addEventListener "storagepersist", ->
+        dataView.setUint8 32, STATE_PERSISTED_HANDLE
+        
+    window.addEventListener "storageroothandle", ->
+        dataView.setUint8 32, STATE_ROOT_HANDLE
+        currentDir = root
+
+        try navigator.storage.persisted().then ( persisted ) ->
+
+            if !persisted
+                dataView.setUint8 32, STATE_UNPERSISTED_HANDLE
+                navigator.storage.persist().then ( persisted ) ->
+                    if  persisted then window.dispatchEvent(
+                        new Event "storagepersist"
+                    )
+            else
+                window.dispatchEvent new Event "storagepersist"
+
+        try navigator.storage.estimate().then ( estimate ) ->
+            dataView.setInt32 16, estimate.quota, lendian
+            dataView.setInt32 20, estimate.usage, lendian            
+
+    init = ->
+        dataView.setUint8 32, STATE_INIT
+        try navigator.storage.getDirectory().then ( handle ) ->
+            if  handle instanceof FileSystemDirectoryHandle
+                handles.push( root = handle )
+                window.dispatchEvent new Event "storageroothandle"
+
+    cd = ( dirName, handle = currentDir ) ->
+        currentDir = await handle.getDirectoryHandle dirName
+
+    mkdir = ( dirName, handle = currentDir ) ->
+        if  dir = await handle.getDirectoryHandle( dirName, { create: true } )
+            emit "storagecreatedirectory", { dir }
+        dir
+
+    touch = ( fileName, handle = currentDir ) ->
+        if  file = await handle.getFileHandle( fileName, { create: true } )
+            emit "storagecreatefile", { file }
+        file
+
+    ls = ( handle = currentDir ) ->
+        iterator = await handle.entries()
+        items = []
+        loop
+            item = await iterator.next()
+            break if item.done is true
+            items.push item.value
+
+        items
+
+    cwd = ( handle = currentDir ) ->
+        await root.resolve handle
+
+    self.mkdir = mkdir
+    self.cwd = cwd
+    self.touch = touch
+    self.handles = handles
+
+    trapArguments = ( args, include = "" ) ->
+
+        list = 'abcdefghijklmnoptrstuvyzqxw'
+        list = list + list.toUpperCase()
+        list = list.split ""
+
+
+        for char in list.slice()
+            for num in [ 0 ... 10 ]
+                list.push char + num
+
+        if  "string" is typeof include
+            include = include.split " "
+
+        for arg in include when !list.includes arg
+            list.push arg 
+
+        parameters = {}
+        for parameter in list
+            parameters[ parameter ] =
+                configurable: on, get : args.push.bind args, parameter
+
+        for k of parameters
+            Object.defineProperty window, k, parameters[k]
+
+        queueMicrotask ->
+            for k of parameters
+                Reflect.deleteProperty window, k
+
+        args
+
+
+    Object.defineProperties window,
+
+        cd : get : ->
+            trapArguments args = [], "up"
+
+            pxy = new Proxy {}, {
+                get : ( o, key ) -> switch true
+                    when (key is Symbol.toPrimitive)
+                        return -> args.push ".." ; 0
+                    when ("string" is typeof key)
+                        args.push key ; 0
+            }
+            
+            queueMicrotask ->
+
+                if  -1 isnt up = args.indexOf "up"
+                    args.splice up, 1
+
+                if  args[0] is ".."
+                    throw /UP/
+
+                await cd args[0]
+
+            pxy
+
+        ls : get : ->
+            trapArguments args = [], "la lh"
+            
+            queueMicrotask ->
+                [ dirName = "" ] = await cwd()
+                ( dirName = "/" + dirName )
+                
+                ls().then ( items ) ->
+
+                    gid = [ "total:", items.length, "path:", dirName ]
+                    console.group gid...
+                    console.log ".."
+
+                    for [ item ] in items
+                        console.log item
+
+                    console.groupEnd gid...
+            
+            1
+
+    do init
+
+    requestIdleCallback ->
+        log(
+            "state:", state()
+            "persisted:", state() >= STATE_PERSISTED_HANDLE
+            "status:", status()
+        )
--- mac.js
+++ mac.js
@@ -1,8 +1,8 @@
-var debug, delay, error, info, log, onbatteryevents, onkeyboardevents, onnetworkevents, onpointerevents, table, warn;
+var battery, debug, delay, error, fs, info, keyboard, log, mouse, netlink, table, warn;
 
 ({log, warn, error, table, debug, info, delay} = console);
 
-(onpointerevents = function() {
+(mouse = function() {
   var changeX, changeY, clientX, clientY, counters, dataView, device, e, iLast, j, lastEvent, len, lendian, offsets, onevent, positions, screenX, screenY;
   device = new ArrayBuffer(64);
   counters = new Int32Array(device, 0, 10);
@@ -30,7 +30,7 @@ var debug, delay, error, info, log, onbatteryevents, onkeyboardevents, onnetwork
   return clientY = dataView.getFloat32.bind(dataView, offsets += 4, lendian);
 })();
 
-(onkeyboardevents = function() {
+(keyboard = function() {
   var activeKey, altKey, counters, ctrlKey, dataView, device, eventType, iEventCount, iKeyDownCount, iKeyUpCount, keyArray, keys, lastChar, lastCode, lastEvent, lendian, metaKey, offset, offsetAltKey, offsetCharCode, offsetCtrlKey, offsetLastEvent, offsetLocation, offsetMetaKey, offsetRepeat, offsetShiftKey, shiftKey;
   device = new ArrayBuffer(144);
   counters = new Int32Array(device, 0, 3);
@@ -100,7 +100,7 @@ var debug, delay, error, info, log, onbatteryevents, onkeyboardevents, onnetwork
   };
 })();
 
-(onbatteryevents = function() {
+(battery = function() {
   var charging, chargingTime, counters, dataView, device, dischargingTime, lastEvent, lendian, level, offsetCTime, offsetDTime, offsetEvent, offsetLevel, offsetState, onevents;
   device = new ArrayBuffer(24);
   counters = new Uint16Array(device, 0, 6);
@@ -137,25 +137,260 @@ var debug, delay, error, info, log, onbatteryevents, onkeyboardevents, onnetwork
   return charging = dataView.getUint8.bind(dataView, offsetState);
 })();
 
-(onnetworkevents = function() {
-  return self.onclick = async function() {
-    var dev;
-    //await navigator.bluetooth.requestDevice({acceptAllDevices: 1})
-    log(1, dev = (await navigator.bluetooth.requestDevice({
-      filters: [
-        {
-          name: "Data-9 iMac"
+(netlink = function() {
+  var changeType, changeTypes, counters, currentState, dataView, device, downlink, effectiveType, ioffline, ionchange, ionline, itype, lendian, rtt, saveData;
+  device = new ArrayBuffer(16);
+  counters = new Int16Array(device, 0, 4);
+  dataView = new DataView(device);
+  lendian = new Uint8Array(Uint16Array.of(1).buffer)[0] === 1;
+  ionchange = 0; //triggers on three events all
+  ioffline = 1;
+  ionline = 2;
+  itype = 3;
+  navigator.connection.onchange = function(e) {
+    ++counters[counters[itype] = ionchange];
+    dataView.setUint8(14, navigator.onLine);
+    dataView.setUint8(13, parseInt(this.effectiveType));
+    dataView.setUint8(12, Number(this.saveData));
+    dataView.setUint16(10, this.rtt, lendian);
+    dataView.setFloat32(8, this.downlink, lendian);
+    return e.preventDefault();
+  };
+  window.addEventListener("offline", function() {
+    return setTimeout(function() {
+      return ++counters[counters[itype] = ioffline];
+    });
+  });
+  window.addEventListener("online", function() {
+    return setTimeout(function() {
+      return ++counters[counters[itype] = ionline];
+    });
+  });
+  changeType = dataView.getUint16.bind(dataView, 6, lendian);
+  downlink = dataView.getFloat32.bind(dataView, 8, lendian);
+  rtt = dataView.getUint16.bind(dataView, 10, lendian);
+  saveData = dataView.getUint8.bind(dataView, 12);
+  effectiveType = dataView.getUint8.bind(dataView, 13);
+  currentState = dataView.getUint8.bind(dataView, 14);
+  return changeTypes = ["linkspeed", "linkdown", "linkup"];
+})();
+
+(fs = function() {
+  var STATE_INIT, STATE_PERSISTED_HANDLE, STATE_ROOT_HANDLE, STATE_UNPERSISTED_HANDLE, STATUS_IDLE, STATUS_READING, STATUS_WRITING, cd, counters, create, currentDir, cwd, dataArray, dataView, device, emit, events, handles, init, lendian, ls, mkdir, quota, read, root, state, status, touch, trapArguments, usage, write;
+  device = new ArrayBuffer(4096 * 256 * 128); //128mb önbellek
+  counters = new Int32Array(device, 0, 4);
+  info = new DataView(device, counters.byteOffset, 4);
+  dataArray = new Uint8Array(device, info.byteOffset);
+  dataView = new DataView(device);
+  lendian = new Uint8Array(Uint16Array.of(1).buffer)[0] === 1;
+  handles = [, ];
+  root = null;
+  currentDir = null;
+  quota = dataView.getInt32.bind(dataView, 12, lendian);
+  usage = dataView.getInt32.bind(dataView, 16, lendian);
+  write = dataView.getInt32.bind(dataView, 20, lendian);
+  read = dataView.getInt32.bind(dataView, 24, lendian);
+  create = dataView.getInt32.bind(dataView, 28, lendian);
+  state = dataView.getUint8.bind(dataView, 32);
+  status = dataView.getUint8.bind(dataView, 34);
+  STATE_INIT = 0;
+  STATE_ROOT_HANDLE = 1;
+  STATE_UNPERSISTED_HANDLE = 2;
+  STATE_PERSISTED_HANDLE = 3;
+  STATUS_IDLE = 0;
+  STATUS_READING = 1;
+  STATUS_WRITING = 2;
+  events = 'onstorageroothandle onstoragepersisted onstoragecreatedirectory storagecreatefile'.split(/\s+|\n/g);
+  emit = function(type, detail = {}) {
+    return window.dispatchEvent(new CustomEvent(type, {detail}));
+  };
+  window.addEventListener("storagecreatedirectory", function({detail}) {
+    dataView.setInt32(28, create() + 1, lendian);
+    return log("onstoragecreatedirectory:", detail);
+  });
+  window.addEventListener("storagecreatefile", function({detail}) {
+    dataView.setInt32(28, create() + 1, lendian);
+    return log("onstoragecreatefile:", detail);
+  });
+  window.addEventListener("storagepersist", function() {
+    return dataView.setUint8(32, STATE_PERSISTED_HANDLE);
+  });
+  window.addEventListener("storageroothandle", function() {
+    dataView.setUint8(32, STATE_ROOT_HANDLE);
+    currentDir = root;
+    try {
+      navigator.storage.persisted().then(function(persisted) {
+        if (!persisted) {
+          dataView.setUint8(32, STATE_UNPERSISTED_HANDLE);
+          return navigator.storage.persist().then(function(persisted) {
+            if (persisted) {
+              return window.dispatchEvent(new Event("storagepersist"));
+            }
+          });
+        } else {
+          return window.dispatchEvent(new Event("storagepersist"));
         }
-      ]
-    })));
-    try {
-      log((await dev.gatt.getPrimaryService()));
+      });
     } catch (error1) {}
     try {
-      log((await dev.gatt.getPrimaryServices()));
+      return navigator.storage.estimate().then(function(estimate) {
+        dataView.setInt32(16, estimate.quota, lendian);
+        return dataView.setInt32(20, estimate.usage, lendian);
+      });
     } catch (error1) {}
+  });
+  init = function() {
+    dataView.setUint8(32, STATE_INIT);
     try {
-      return log((await dev.gatt.connect()));
+      return navigator.storage.getDirectory().then(function(handle) {
+        if (handle instanceof FileSystemDirectoryHandle) {
+          handles.push(root = handle);
+          return window.dispatchEvent(new Event("storageroothandle"));
+        }
+      });
     } catch (error1) {}
   };
+  cd = async function(dirName, handle = currentDir) {
+    return currentDir = (await handle.getDirectoryHandle(dirName));
+  };
+  mkdir = async function(dirName, handle = currentDir) {
+    var dir;
+    if (dir = (await handle.getDirectoryHandle(dirName, {
+      create: true
+    }))) {
+      emit("storagecreatedirectory", {dir});
+    }
+    return dir;
+  };
+  touch = async function(fileName, handle = currentDir) {
+    var file;
+    if (file = (await handle.getFileHandle(fileName, {
+      create: true
+    }))) {
+      emit("storagecreatefile", {file});
+    }
+    return file;
+  };
+  ls = async function(handle = currentDir) {
+    var item, items, iterator;
+    iterator = (await handle.entries());
+    items = [];
+    while (true) {
+      item = (await iterator.next());
+      if (item.done === true) {
+        break;
+      }
+      items.push(item.value);
+    }
+    return items;
+  };
+  cwd = async function(handle = currentDir) {
+    return (await root.resolve(handle));
+  };
+  self.mkdir = mkdir;
+  self.cwd = cwd;
+  self.touch = touch;
+  self.handles = handles;
+  trapArguments = function(args, include = "") {
+    var arg, char, j, k, l, len, len1, len2, list, m, n, num, parameter, parameters, ref;
+    list = 'abcdefghijklmnoptrstuvyzqxw';
+    list = list + list.toUpperCase();
+    list = list.split("");
+    ref = list.slice();
+    for (j = 0, len = ref.length; j < len; j++) {
+      char = ref[j];
+      for (num = l = 0; l < 10; num = ++l) {
+        list.push(char + num);
+      }
+    }
+    if ("string" === typeof include) {
+      include = include.split(" ");
+    }
+    for (m = 0, len1 = include.length; m < len1; m++) {
+      arg = include[m];
+      if (!list.includes(arg)) {
+        list.push(arg);
+      }
+    }
+    parameters = {};
+    for (n = 0, len2 = list.length; n < len2; n++) {
+      parameter = list[n];
+      parameters[parameter] = {
+        configurable: true,
+        get: args.push.bind(args, parameter)
+      };
+    }
+    for (k in parameters) {
+      Object.defineProperty(window, k, parameters[k]);
+    }
+    queueMicrotask(function() {
+      var results;
+      results = [];
+      for (k in parameters) {
+        results.push(Reflect.deleteProperty(window, k));
+      }
+      return results;
+    });
+    return args;
+  };
+  Object.defineProperties(window, {
+    cd: {
+      get: function() {
+        var args, pxy;
+        trapArguments(args = [], "up");
+        pxy = new Proxy({}, {
+          get: function(o, key) {
+            switch (true) {
+              case key === Symbol.toPrimitive:
+                return function() {
+                  args.push("..");
+                  return 0;
+                };
+              case "string" === typeof key:
+                args.push(key);
+                return 0;
+            }
+          }
+        });
+        queueMicrotask(async function() {
+          var up;
+          if (-1 !== (up = args.indexOf("up"))) {
+            args.splice(up, 1);
+          }
+          if (args[0] === "..") {
+            throw /UP/;
+          }
+          return (await cd(args[0]));
+        });
+        return pxy;
+      }
+    },
+    ls: {
+      get: function() {
+        var args;
+        trapArguments(args = [], "la lh");
+        queueMicrotask(async function() {
+          var dirName;
+          [dirName = ""] = (await cwd());
+          (dirName = "/" + dirName);
+          return ls().then(function(items) {
+            var gid, item, j, len;
+            gid = ["total:", items.length, "path:", dirName];
+            console.group(...gid);
+            console.log("..");
+            for (j = 0, len = items.length; j < len; j++) {
+              [item] = items[j];
+              console.log(item);
+            }
+            return console.groupEnd(...gid);
+          });
+        });
+        return 1;
+      }
+    }
+  });
+  init();
+  return requestIdleCallback(function() {
+    return log("state:", state(), "persisted:", state() >= STATE_PERSISTED_HANDLE, "status:", status());
+  });
 })();
